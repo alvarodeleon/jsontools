5ada9c100e9a73899c9b08967802f380d34d18c9
Alvaro <9125409+alvarodeleon@users.noreply.github.com>
2021-03-22 22:24:45 -0300

    v0.0.7

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@
 from setuptools import setup, find_packages
 setup(
   name = 'jsonhelper',
-  version = '0.0.5',
+  version = '0.0.7',
   description = 'JSON Tools',
   author = 'Alvaro De Leon',
   author_email = 'deleon@adl.com.uy',
   url = 'https://github.com/alvarodeleon/jsontools', # use the URL to the github repo
-  download_url = 'https://github.com/alvarodeleon/jsontools/tarball/0.0.5',
+  download_url = 'https://github.com/alvarodeleon/jsontools/tarball/0.0.7',
   keywords = ['json'],
   classifiers = [],
   package_dir = {'':'src'},
